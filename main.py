361f3fad9354bba88dfb3afe3f3611893a0a8e2e
Ivan Florentina <ciobanue016@gmail.com>
2025-03-11 21:04:00 +0200

    Remove from main display info and total sum

--- main.py
+++ main.py
@@ -3,10 +3,8 @@ from product_manager import ProductManager
 
 manager = ProductManager()
 
-manager.add_product("Headphones", 400)
+manager.add_product("Headphones", 250)
 
-manager.add_product("Monitor", 1300)
+manager.add_product("Monitor", 2500)
 
-manager.display_products()
 
-manager.total_sum()
